7712be258e243961c1019a6435b5f07268311f38
Michael Mussato <michimussato@gmail.com>
2025-04-05 11:43:03 +0200

    refactor

---
+++ readme_generator.py
@@ -0,0 +1,7 @@
+from OpenStudioLandscapes.engine.utils import markdown
+
+
+if __name__ == "__main__":
+    from OpenStudioLandscapes.Dagster import constants
+
+    markdown.main(constants=constants)
