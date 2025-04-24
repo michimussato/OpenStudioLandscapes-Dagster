824a23b867ff23ec2ec7b80cf010481f1a2c8eee
Michael Mussato <michimussato@gmail.com>
2025-04-24 10:58:42 +0200

    refactor

--- src/OpenStudioLandscapes/Dagster/constants.py
+++ src/OpenStudioLandscapes/Dagster/constants.py
@@ -16,15 +16,12 @@ from dagster import (
     AssetExecutionContext,
     Output,
     MetadataValue,
-    AssetsDefinition,
-    AssetKey,
     get_dagster_logger,
 )
 
 LOGGER = get_dagster_logger(__name__)
 
 from OpenStudioLandscapes.engine.utils import *
-from OpenStudioLandscapes.engine.base.ops import op_constants
 from OpenStudioLandscapes.engine.constants import DOCKER_USE_CACHE_GLOBAL
 from OpenStudioLandscapes.engine.enums import OpenStudioLandscapesConfig
 
@@ -117,21 +114,8 @@ FEATURE_CONFIGS = {
 # @formatter:on
 
 
-constants = AssetsDefinition.from_op(
-    op_constants,
-    can_subset=False,
-    group_name=GROUP,
-    keys_by_input_name={
-        "group_in": AssetKey([*ASSET_HEADER["key_prefix"], "group_in"]),
-        "NAME": AssetKey([*ASSET_HEADER["key_prefix"], "NAME"]),
-    },
-    keys_by_output_name={
-        "COMPOSE_SCOPE": AssetKey([*ASSET_HEADER["key_prefix"], "COMPOSE_SCOPE"]),
-        "FEATURE_CONFIG": AssetKey([*ASSET_HEADER["key_prefix"], "FEATURE_CONFIG"]),
-    }
-)
-
-
+# Todo:
+#  - [ ] move to common_assets
 @multi_asset(
     name=f"constants_{GROUP}",
     outs={
