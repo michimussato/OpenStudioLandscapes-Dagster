3c16571f3a9b86d86b63056a605120a9cbc3decf
Michael Mussato <michimussato@gmail.com>
2025-09-24 14:42:56 +0200

    - feature-openstudiolandscapes-teleport: nox and nox readme

--- src/OpenStudioLandscapes/Dagster/assets.py
+++ src/OpenStudioLandscapes/Dagster/assets.py
@@ -661,7 +661,9 @@ def compose_dagster(
 
     service_name = "dagster"
     container_name = "--".join([service_name, env.get("LANDSCAPE", "default")])
-    host_name = ".".join([env["HOSTNAME"] or service_name, env["OPENSTUDIOLANDSCAPES__DOMAIN_LAN"]])
+    host_name = ".".join(
+        [env["HOSTNAME"] or service_name, env["OPENSTUDIOLANDSCAPES__DOMAIN_LAN"]]
+    )
 
     docker_dict = {
         "services": {
