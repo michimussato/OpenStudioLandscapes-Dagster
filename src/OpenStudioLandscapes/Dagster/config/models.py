698c6332e6b57719074c5cf109f9c70653d5ad8d
Michael Mussato <michimussato@gmail.com>
2026-04-26 17:52:02 +0200

    add computed_field to properties

--- src/OpenStudioLandscapes/Dagster/config/models.py
+++ src/OpenStudioLandscapes/Dagster/config/models.py
@@ -5,6 +5,7 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
+    computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -138,6 +139,7 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
+    @computed_field
     @property
     def dagster_postgres_db_install_dir_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
