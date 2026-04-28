daf73ffb218a9c4eb16724a3ee58ef71a2cf1376
Michael Mussato <michimussato@gmail.com>
2026-04-28 10:56:54 +0200

    remove compute_fields. serializing them causes problems and is also not needed.

--- src/OpenStudioLandscapes/Dagster/config/models.py
+++ src/OpenStudioLandscapes/Dagster/config/models.py
@@ -5,7 +5,6 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
-    # computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -139,7 +138,6 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
-    # @computed_field
     @property
     def dagster_postgres_db_install_dir_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
