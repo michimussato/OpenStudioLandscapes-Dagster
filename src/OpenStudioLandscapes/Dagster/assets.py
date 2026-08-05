0f686c3faf6c24471416dd8cf2a49482909b2f63
Michael Mussato <michimussato@gmail.com>
2026-08-05 14:56:54 +0200

    first test with docker as configurable resource

--- src/OpenStudioLandscapes/Dagster/assets.py
+++ src/OpenStudioLandscapes/Dagster/assets.py
@@ -27,8 +27,9 @@ from OpenStudioLandscapes.engine.common_assets import (
     group_in,
     group_out,
 )
-from OpenStudioLandscapes.engine.config.models import ConfigEngine, DockerConfigModel
+from OpenStudioLandscapes.engine.config.models import ConfigEngine
 from OpenStudioLandscapes.engine.base.configurable_resources.docker_registry_resource import DockerRegistryConfigurableResource
+from OpenStudioLandscapes.engine.base.configurable_resources.docker_resource import DockerConfigurableResource
 from OpenStudioLandscapes.engine.constants import (
     ASSET_HEADER_BASE,
     ConfigParent,
@@ -129,6 +130,7 @@ feature_in_parent: Union[AssetsDefinition, None] = group_in.get_feature_in_paren
 def write_dockerfile(
     context: AssetExecutionContext,
     config_DockerRegistryConfigurableResource: DockerRegistryConfigurableResource,
+    config_DockerConfigurableResource: DockerConfigurableResource,
     feature_in: OpenStudioLandscapesFeatureIn,  # pylint: disable=redefined-outer-name
     CONFIG: config.models.Config,  # pylint: disable=redefined-outer-name
 ) -> Generator[Output[pathlib.Path] | AssetMaterialization, None, None]:
@@ -138,7 +140,7 @@ def write_dockerfile(
 
     config_engine: ConfigEngine = CONFIG.config_engine
 
-    docker_config: DockerConfigModel = config_engine.openstudiolandscapes__docker_config
+    docker_config: DockerConfigurableResource = config_DockerConfigurableResource
 
     docker_image: Dict = feature_in.openstudiolandscapes_base.docker_image_base
     context.log.debug(f"{docker_image = }")
@@ -261,6 +263,7 @@ def write_dockerfile(
 def build_docker_image(
     context: AssetExecutionContext,
     config_DockerRegistryConfigurableResource: DockerRegistryConfigurableResource,
+    config_DockerConfigurableResource: DockerConfigurableResource,
     feature_in: OpenStudioLandscapesFeatureIn,  # pylint: disable=redefined-outer-name
     CONFIG: config.models.Config,  # pylint: disable=redefined-outer-name
     write_dockerfile: pathlib.Path,  # pylint: disable=redefined-outer-name
@@ -275,7 +278,7 @@ def build_docker_image(
 
     config_engine: ConfigEngine = CONFIG.config_engine
 
-    docker_config: DockerConfigModel = config_engine.openstudiolandscapes__docker_config
+    docker_config: DockerConfigurableResource = config_DockerConfigurableResource
 
     docker_image: Dict = feature_in.openstudiolandscapes_base.docker_image_base
     context.log.debug(f"{docker_image = }")
